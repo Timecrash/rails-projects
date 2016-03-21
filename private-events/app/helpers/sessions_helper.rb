ee8f92a676a3d74730eb7c382f7879e7c9833045
Timecrash <mzvelez75@gmail.com>
2016-03-21 17:11:00 +0000

    Initialize Event model

--- private-events/app/helpers/sessions_helper.rb
+++ private-events/app/helpers/sessions_helper.rb
@@ -1,2 +1,25 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+    self.current_user = user
+  end
+  
+  def log_out
+    self.current_user = nil
+    session.delete(:user_id)
+  end
+  
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find(user_id)
+    end
+  end
+  
+  def current_user=(user)
+    @current_user = user
+  end
+  
+  def logged_in?
+    !current_user.nil?
+  end
 end
