85efca84252ceef6e08a1883ac0ba1643c3ab816
Timecrash <mzvelez75@gmail.com>
2016-03-23 12:22:13 +0000

    Delete static_pages

--- private-events/config/routes.rb
+++ private-events/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root                 'static_pages#home'
+  root                 'events#index'
   get     'login'   => 'sessions#new'
   post    'login'   => 'sessions#create'
   delete  'logout'  => 'sessions#destroy'
