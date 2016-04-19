e638448c79b756ace16cc8b7b827e44a4658844c
Timecrash <mzvelez75@gmail.com>
2016-04-19 14:36:31 +0000

    Finish project

--- flickr-sidebar/app/controllers/static_pages_controller.rb
+++ flickr-sidebar/app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
   def index
+    @user_id     = params[:user_id]     || "15261150@N08"
+    @photo_count = params[:photo_count] || 12
   end
 end
--- flickr-sidebar/app/helpers/flickr_helper.rb
+++ flickr-sidebar/app/helpers/flickr_helper.rb
@@ -1,11 +1,11 @@
 module FlickrHelper
-  def user_photos(user_id, photo_count = 12)
+  def user_photos(user_id, photo_count)
     flickr.photos.search(user_id: user_id, per_page: photo_count)
   end
   
-  def render_flickr_sidebar_widget(user_id, photo_count = 12)
+  def render_flickr(user_id, photo_count)
     photos = user_photos(user_id, photo_count)
-    render partial: '/flickr/sidebar_widget', locals: { photos: photos }
+    render partial: '/flickr/sidebar_widget', locals: { photos: photos, user_id: user_id }
     rescue Exception
       render partial: '/flickr/unavailable'
   end
