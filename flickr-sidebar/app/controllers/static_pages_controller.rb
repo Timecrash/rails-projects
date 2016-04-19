88069f7bea167a034d7db4778df20eebc97962c8
Timecrash <mzvelez75@gmail.com>
2016-04-19 14:44:35 +0000

    Refactor so the controller does more work

--- flickr-sidebar/app/controllers/static_pages_controller.rb
+++ flickr-sidebar/app/controllers/static_pages_controller.rb
@@ -2,5 +2,6 @@ class StaticPagesController < ApplicationController
   def index
     @user_id     = params[:user_id]     || "15261150@N08"
     @photo_count = params[:photo_count] || 12
+    @user        = flickr.people.getInfo(user_id: @user_id)
   end
 end
