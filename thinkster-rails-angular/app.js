8df6fce0a0853509fe56db3e7d234cb69d9b678c
Timecrash <mzvelez75@gmail.com>
2016-08-01 15:51:00 +0000

    Begin working on app

---
+++ thinkster-rails-angular/app.js
@@ -0,0 +1,14 @@
+angular.module('flapperNews', [])
+.controller('MainCtrl', ['$scope', function($scope) {
+  $scope.test = "Hello world!";
+  $scope.posts = [
+    { title: 'post 1', upvotes: 5 },
+    { title: 'post 2', upvotes: 2 },
+    { title: 'post 3', upvotes: 15 },
+    { title: 'post 4', upvotes: 9 },
+    { title: 'post 5', upvotes: 4 }];
+  $scope.addPost = function() {
+    $scope.posts.push( { title: 'A new post!', upvotes: 0 } );
+  };
+}
+]);
