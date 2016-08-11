e65868cd1f86c9351e46c7fac6ea4bbac425c391
Timecrash <mzvelez75@gmail.com>
2016-08-11 12:30:01 +0000

    Adding some style

--- thinkster-rails-angular/app.js
+++ thinkster-rails-angular/app.js
@@ -7,8 +7,22 @@ angular.module('flapperNews', [])
     { title: 'post 3', upvotes: 15 },
     { title: 'post 4', upvotes: 9 },
     { title: 'post 5', upvotes: 4 }];
+  
   $scope.addPost = function() {
-    $scope.posts.push( { title: 'A new post!', upvotes: 0 } );
+    if (!$scope.title || $scope.title === "") {
+      return;
+    } else {
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0 });
+      $scope.title = "";
+      $scope.link = "";
+    }
+  };
+  
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
   };
 }
 ]);
