6911389571404a773c90eebe114c33a74f651bd9
Timecrash <mzvelez75@gmail.com>
2016-09-02 15:48:23 +0000

    Update gitignore to ignore /spring

--- flapper-news/app/controllers/application_controller.rb
+++ flapper-news/app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :json
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   
   def angular
     render 'layouts/application'
@@ -11,6 +12,6 @@ class ApplicationController < ActionController::Base
   
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 end
